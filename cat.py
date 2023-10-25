3f2c7603a0e1dc854144449aa211192feef6e3ee
NonCapisco <mkololli@uni-osnabrueck.de>
2023-10-25 13:40:50 +0200

    changed the greeting in cat.py

--- cat.py
+++ cat.py
@@ -11,4 +11,4 @@ class Cat:
         self.name = name
         
     def greet(self, other_cat):
-        print(f"Hello, I am {self.name}! I see you are also a cool fluffy kitty {other_cat.name}, let’s together purr at the human, so that they shall give us food.")
+        print(f"Hellooo, I am {self.name}! I see you are also a cool fluffy kitty {other_cat.name}, let’s together purr at the human, so that they shall give us food.")
